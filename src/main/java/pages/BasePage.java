fe84867db08622d91f7cd2d3c41a77de5e97770a
emsan2210 <emo2210@abv.bg>
2025-03-03 18:38:18 +0200

    fix(BasePage): add wait variable declaration

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -10,6 +10,7 @@ import java.time.Duration;
 
 public abstract class BasePage {
     protected WebDriver driver;
+    protected WebDriverWait wait;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
