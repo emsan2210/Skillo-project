27cdfd526f9c0ca8c46454228a3017314b1ce4f7
emsan2210 <emo2210@abv.bg>
2025-03-03 19:47:27 +0200

    feat(BaseTest): make driver OS independent

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 import java.time.Duration;
 
 public abstract class BaseTest {
@@ -11,10 +14,14 @@ public abstract class BaseTest {
 
     @BeforeMethod
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver-win64\\chromedriver.exe");
+        // Automatically setup ChromeDriver for all OS
+        WebDriverManager.chromedriver().setup();
+
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // Open the website
         driver.get("http://training.skillo-bg.com:4300/posts/all");
     }
 
@@ -25,4 +32,3 @@ public abstract class BaseTest {
         }
     }
 }
-
